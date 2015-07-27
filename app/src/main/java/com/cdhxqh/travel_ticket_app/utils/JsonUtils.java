d3caca3c7f1fb5dd7883e8d0bccfe7ac8d7dcbcc
wxs19920926 <425767281@qq.com>
2015-07-27 16:45:02 +0800

    提交登录界面

--- app/src/main/java/com/cdhxqh/travel_ticket_app/utils/JsonUtils.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/utils/JsonUtils.java
@@ -37,5 +37,60 @@ public class JsonUtils {
         }
     }
 
+    /**邮箱验证**/
+    public static int parsingEmailStr(final Context cxt, String data) {
+        try {
+            JSONObject json = new JSONObject(data);
+            String errcode = json.getString("errcode");
+            Log.i(TAG,"errcode="+errcode);
+            if (errcode.equals(Constants.SUCCESS_EMAIL)) {
+                return 1; //成功
+            } else if(errcode.equals(Constants.FAILURE_EMAIL)){
+                return 2; //邮箱未注册
+            } else {
+                return 3; //邮箱注册失败
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
+    /**获取验证码**/
+    public static int parsingPhoneCode(final Context cxt, String data) {
+        try {
+            JSONObject json = new JSONObject(data);
+            String errcode = json.getString("errcode");
+            Log.i(TAG, "errcode=" + errcode);
+            if (errcode.equals(Constants.SUCCESS_LINE)) {
+                return 1; //成功
+            } else {
+                return 0; //成功
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    /**获取验证码**/
+    public static int parsingLinePass(final Context cxt, String data) {
+        try {
+            JSONObject json = new JSONObject(data);
+            String errcode = json.getString("errcode");
+            Log.i(TAG, "errcode=" + errcode);
+            if (errcode.equals(Constants.SUCCESS_LINE_PASS)) {
+                return 1; //成功
+            }else if(errcode.equals(Constants.RUNTIME_LINE_PASS)){
+                return 2; //验证码失效
+            } else if(errcode.equals(Constants.RUNTIME_LINE_PASS)){
+                return 3; //验证码验证失败
+            } else {
+                return 0;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
