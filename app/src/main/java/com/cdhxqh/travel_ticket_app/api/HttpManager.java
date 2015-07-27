9f73341095e0f9e77ccb34f9caf5ed819874cc74
wxs19920926 <425767281@qq.com>
2015-07-27 15:53:42 +0800

    提交登录界面

--- app/src/main/java/com/cdhxqh/travel_ticket_app/api/HttpManager.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/api/HttpManager.java
@@ -37,7 +37,7 @@ public class HttpManager {
         mapparams.put("password",password);
 
 
-        requestOnceWithURLString(cxt, mapparams, new HttpRequestHandler<String>() {
+        requestOnceWithURLString(cxt, Constants.LOGIN_URL, mapparams, new HttpRequestHandler<String>() {
             @Override
             public void onSuccess(String data, int totalPages, int currentPage) {
                 Log.i(TAG, "data=" + data);
@@ -65,6 +65,127 @@ public class HttpManager {
         });
     }
 
+    /**
+     * 使用邮箱号获取密码
+     *
+     * @param cxt
+     * @param mailCode 邮箱号
+     * @param handler  返回结果处理
+     */
+    public static void getMailPassword(final Context cxt,final String mailCode,
+                                         final HttpRequestHandler<Integer> handler) {
+        Map<String, String> mapparams=new HashMap<String,String>();
+        mapparams.put("email",mailCode);
+
+        requestOnceWithURLString(cxt, Constants.PHONEPASS_URL, mapparams, new HttpRequestHandler<String>() {
+            @Override
+            public void onSuccess(String data, int totalPages, int currentPage) {
+                Log.i(TAG, "data=" + data);
+            }
+
+            @Override
+            public void onSuccess(String data) {
+                Log.i(TAG, "data=" + data);
+                //解析返回的Json数据
+                int code = JsonUtils.parsingEmailStr(cxt, data);
+//
+                if (code == 1) {
+                    SafeHandler.onSuccess(handler, 200);
+                } else if(code ==2) {
+                    SafeHandler.onFailure(handler, "该邮箱未注册");
+                } else if(code == 3) {
+                    SafeHandler.onFailure(handler, "邮箱发送失败");
+                }
+
+            }
+
+
+            @Override
+            public void onFailure(String error) {
+                SafeHandler.onFailure(handler, error);
+            }
+        });
+    }
+
+    /**
+     * 使用手机号获取验证码
+     *
+     * @param cxt
+     * @param phoneNumber 手机号
+     * @param handler  返回结果处理
+     */
+    public static void getPhoneCode(final Context cxt,final String phoneNumber,
+                                       final HttpRequestHandler<Integer> handler) {
+        Map<String, String> mapparams=new HashMap<String,String>();
+        mapparams.put("mobilePhone",phoneNumber);
+
+        requestOnceWithURLString(cxt, Constants.PHONEPASS_URL, mapparams, new HttpRequestHandler<String>() {
+            @Override
+            public void onSuccess(String data, int totalPages, int currentPage) {
+                Log.i(TAG, "data=" + data);
+            }
+
+            @Override
+            public void onSuccess(String data) {
+                Log.i(TAG, "data=" + data);
+                //解析返回的Json数据
+                int code = JsonUtils.parsingPhoneCode(cxt, data);
+//
+                if (code == 1) {
+                    SafeHandler.onSuccess(handler, 200);
+                } else if(code ==2) {
+                    SafeHandler.onFailure(handler, "验证码失效");
+                } else if(code == 3) {
+                    SafeHandler.onFailure(handler, "验证失败");
+                }
+
+            }
+
+
+            @Override
+            public void onFailure(String error) {
+                SafeHandler.onFailure(handler, error);
+            }
+        });
+    }
+    /**
+     * 使用验证码获取新密码
+     *
+     * @param cxt
+     * @param phoneNumber 验证码
+     * @param handler  返回结果处理
+     */
+    public static void getPhonePass(final Context cxt,final String phoneNumber,
+                                    final HttpRequestHandler<Integer> handler) {
+        Map<String, String> mapparams=new HashMap<String,String>();
+        mapparams.put("authstring",phoneNumber);
+
+        requestOnceWithURLString(cxt, Constants.PHONELINE_URL, mapparams, new HttpRequestHandler<String>() {
+            @Override
+            public void onSuccess(String data, int totalPages, int currentPage) {
+                Log.i(TAG, "data=" + data);
+            }
+
+            @Override
+            public void onSuccess(String data) {
+                Log.i(TAG, "data=" + data);
+                //解析返回的Json数据
+                int code = JsonUtils.parsingLinePass(cxt, data);
+//
+                if (code == 1) {
+                    SafeHandler.onSuccess(handler, 200);
+                }
+
+            }
+
+
+            @Override
+            public void onFailure(String error) {
+                SafeHandler.onFailure(handler, error);
+            }
+        });
+    }
+
 
     /**
      * 公用的URl连接
@@ -74,7 +195,7 @@ public class HttpManager {
      * @param handler *
      */
 
-    private static void requestOnceWithURLString(final Context cxt, Map<String, String> mapparams,
+    private static void requestOnceWithURLString(final Context cxt, String url, Map<String, String> mapparams,
                                                  final HttpRequestHandler<String> handler) {
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
@@ -82,11 +203,11 @@ public class HttpManager {
             params.put(entry.getKey(),entry.getValue());
         }
 
-        client.post(Constants.LOGIN_URL, params, new TextHttpResponseHandler() {
+        client.post(url, params, new TextHttpResponseHandler() {
 
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+               public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 Log.i(TAG, "FstatusCode=" + statusCode);
                 SafeHandler.onFailure(handler, ErrorType.errorMessage(cxt, ErrorType.ErrorLoginFailure));
             }
