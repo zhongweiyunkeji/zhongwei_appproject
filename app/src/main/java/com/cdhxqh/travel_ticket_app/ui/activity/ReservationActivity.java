faf28f0e9cbd400c76c34aaf5bfef79321a23a4b
wxs19920926 <425767281@qq.com>
2015-07-28 13:35:04 +0800

    预定信息class

---
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/ReservationActivity.java
@@ -0,0 +1,29 @@
+package com.cdhxqh.travel_ticket_app.ui.activity;
+
+import android.os.Bundle;
+
+import com.cdhxqh.travel_ticket_app.R;
+
+/**
+ * Created by Administrator on 2015/7/28.
+ */
+public class ReservationActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+        findViewById();
+        initView();
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
