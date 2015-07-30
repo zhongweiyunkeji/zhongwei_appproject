5f8496442b326aaeecd6937a35a362e45c0ebde0
wxs19920926 <425767281@qq.com>
2015-07-30 16:42:03 +0800

    修复bug

--- app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/PhoneActivity.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/PhoneActivity.java
@@ -186,10 +186,7 @@ public class PhoneActivity extends BaseActivity {
     private View.OnClickListener backImageViewOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-//            finish();
-            Intent intent = new Intent();
-            intent.setClass(PhoneActivity.this, IntroductionActivity.class);
-            startActivityForResult(intent, 0);
+            finish();
         }
     };
 
