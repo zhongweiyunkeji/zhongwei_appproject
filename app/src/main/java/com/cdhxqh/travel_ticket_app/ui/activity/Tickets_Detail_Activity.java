230f673869f9ec6f779188d2561ae79241df8107
taocaocajaini <278233503@qq.cpm>
2015-07-31 21:26:25 +0800

    景点门票详情界面

--- app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/Tickets_Detail_Activity.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/Tickets_Detail_Activity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cdhxqh.travel_ticket_app.R;
 import com.cdhxqh.travel_ticket_app.model.Ecs_brand;
+import com.cdhxqh.travel_ticket_app.ui.adapter.Tickets_ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 景区门票详情
@@ -23,6 +28,16 @@ public class Tickets_Detail_Activity extends BaseActivity {
 
     /**Ecs_brand**/
     Ecs_brand ecs_brand;
+
+    /**可扩展的**/
+    ExpandableListView expandableListView;
+
+    Tickets_ExpandableListAdapter tickets_expandableListAdapter;
+
+
+    private List<String> group;
+    private List<List<String>> child;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +58,7 @@ public class Tickets_Detail_Activity extends BaseActivity {
     protected void findViewById() {
         backImageView=(ImageView)findViewById(R.id.ticket_detail_back_id);
         titleTextView=(TextView)findViewById(R.id.ticket_detail_title_id);
+        expandableListView=(ExpandableListView)findViewById(R.id.expandableListView);
     }
 
     @Override
@@ -52,6 +68,9 @@ public class Tickets_Detail_Activity extends BaseActivity {
         }
 
         backImageView.setOnClickListener(backImageViewOnClickListener);
+        initData();
+        tickets_expandableListAdapter=new Tickets_ExpandableListAdapter(Tickets_Detail_Activity.this,group,child);
+        expandableListView.setAdapter(tickets_expandableListAdapter);
     }
 
 
@@ -80,6 +99,26 @@ public class Tickets_Detail_Activity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void initData() {
+        group = new ArrayList<String>();
+        child = new ArrayList<List<String>>();
+        addInfo("成人票",new String[]{"沙坡头1","沙坡头2","沙坡头3","沙坡头4"});
+        addInfo("儿童票", new String[]{"儿童票5","儿童票6","儿童票7̨"});
+        addInfo("优待票", new String[]{"儿童票8","儿童票","儿童票10"});
+    }
+
+
+    private void addInfo(String g,String[] c) {
+        group.add(g);
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < c.length; i++) {
+            list.add(c[i]);
+        }
+        child.add(list);
     }
 
 
