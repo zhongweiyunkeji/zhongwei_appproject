472232c34b9283bf3fe0cf3717fed87b7c5b9dd2
wxs19920926 <425767281@qq.com>
2015-08-29 23:33:46 +0800

    aa

--- app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/BaseActivity.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/ui/activity/BaseActivity.java
@@ -37,7 +37,7 @@ public abstract class BaseActivity extends Activity implements AccountUtils.OnAc
     protected InputMethodManager imm;
     private TelephonyManager tManager;
 
-    public boolean userType;
+    public boolean userType = true;
 
     protected boolean mIsLogin;
     protected Ec_user ec_user;
