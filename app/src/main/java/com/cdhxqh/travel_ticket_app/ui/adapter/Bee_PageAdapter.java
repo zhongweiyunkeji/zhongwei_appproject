f69a9b14be6eca2b53d0d39b0ce38ecd0de1a09b
hexian <1336125942@qq.com>
2015-09-17 13:47:56 +0800

    增加banner轮播效果

--- app/src/main/java/com/cdhxqh/travel_ticket_app/ui/adapter/Bee_PageAdapter.java
+++ app/src/main/java/com/cdhxqh/travel_ticket_app/ui/adapter/Bee_PageAdapter.java
@@ -17,24 +17,40 @@ public class Bee_PageAdapter extends PagerAdapter
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) 	{
-        container.removeView(mListViews.get(position));//删除页卡
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        int index = position%mListViews.size();
+        container.removeView(mListViews.get(index));//删除页卡
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {	//这个方法用来实例化页卡
-        container.addView(mListViews.get(position), 0);//添加页卡
-        return mListViews.get(position);
+        int index = position%mListViews.size();
+        View view = mListViews.get(index);
+        removeParent(view);
+        container.addView(view);//添加页卡
+        return view;
     }
 
     @Override
     public int getCount() {
-        return  mListViews.size();//返回页卡的数量
+        return  Integer.MAX_VALUE;//返回页卡的数量
+        // return  mListViews.size();//返回页卡的数量
     }
 
     @Override
     public boolean isViewFromObject(View arg0, Object arg1) {
         return arg0==arg1;//官方提示这样写
+    }
+
+    public int getRealCount(){
+        return  mListViews.size();//返回页卡的数量
+    }
+
+    private void removeParent(View view){
+        ViewGroup parent = (ViewGroup)view.getParent();
+        if(parent!=null){
+            parent.removeView(view);
+        }
     }
 
 }
